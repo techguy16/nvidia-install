71cfeff92e7560842907989114d7e96f5c5e2505
techguy16 <88870951+techguy16@users.noreply.github.com>
2023-02-18 09:44:10 +1300

    Update nvidia-install.sh

--- nvidia-install.sh
+++ nvidia-install.sh
@@ -1,22 +1,44 @@
 cmd=(dialog --keep-tite --menu "Select your NVIDIA Card's Series" 22 76 16)
 
-options=(1 "GT 500 Series"
-         2 "GeForce 5 FX Series")
+options=(1 "Nvidia Driver 173.14.39 (x64)"
+         2 "Nvidia Driver 390.157 (x64)"
+         3 "Nvidia Driver 418.113 (x64)"
+         4 "Nvidia Driver 470.161.03 (x64)"
+         5 "Nvidia Driver 525.89.02 (x64)")
 
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 
 for choice in $choices
 do
     case $choice in
-        1)
-            wget https://us.download.nvidia.com/XFree86/Linux-x86_64/390.157/NVIDIA-Linux-x86_64-390.157.run
-            sudo chmod +x NVIDIA-Linux-x86_64-390.157.run
-            sudo ./NVIDIA-Linux-x86_64-390.157.run
-            ;;
-        2)
+        1)  
             wget https://us.download.nvidia.com/XFree86/Linux-x86_64/173.14.39/NVIDIA-Linux-x86_64-173.14.39-pkg2.run
             sudo chmod +x NVIDIA-Linux-x86_64-173.14.39-pkg2.run
             sudo ./NVIDIA-Linux-x86_64-173.14.39-pkg2.run
             ;;
+        2)
+            wget https://us.download.nvidia.com/XFree86/Linux-x86_64/390.157/NVIDIA-Linux-x86_64-390.157.run
+            sudo chmod +x NVIDIA-Linux-x86_64-390.157.run
+            sudo ./NVIDIA-Linux-x86_64-390.157.run
+            ;;
+        3)
+            wget https://us.download.nvidia.com/XFree86/Linux-x86_64/418.113/NVIDIA-Linux-x86_64-418.113.run
+            sudo chmod +x NVIDIA-Linux-x86_64-418.113.run
+            sudo ./NVIDIA-Linux-x86_64-418.113.run
+            ;;
+        4)
+            wget https://us.download.nvidia.com/XFree86/Linux-x86_64/470.161.03/NVIDIA-Linux-x86_64-470.161.03.run
+            sudo chmod +x NVIDIA-Linux-x86_64-470.161.03.run
+            sudo ./NVIDIA-Linux-x86_64-470.161.03.run
+            ;;
+        5)
+            wget https://us.download.nvidia.com/XFree86/Linux-x86_64/525.89.02/NVIDIA-Linux-x86_64-525.89.02.run
+            sudo chmod +x NVIDIA-Linux-x86_64-525.89.02.run
+            sudo ./NVIDIA-Linux-x86_64-525.89.02.run
+            ;;
     esac
 done
+
+echo "Blacklisting nouveau..."
+sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
